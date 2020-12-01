fcd5122d77fee9c3a90b21f498f54a281dbb8d75
Clayton Salem <csalem@unleashed-technologies.com>
2020-12-01 10:10:19 -0500

    Initial commit + first solution

---
+++ Tests/advent-of-codeTests/advent_of_codeTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import advent_of_code
+
+final class advent_of_codeTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(advent_of_code().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
