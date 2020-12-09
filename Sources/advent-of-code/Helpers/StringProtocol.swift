ba85c589c125ed50224a2987c3a605baacc1ecd0
Clayton Salem <claytonjsalem@gmail.com>
2020-12-08 21:50:32 -0500

    Supporting regex

--- Sources/advent-of-code/Helpers/StringProtocol.swift
+++ Sources/advent-of-code/Helpers/StringProtocol.swift
@@ -1,16 +1,20 @@
 import Foundation
 
 extension StringProtocol {
-    func index<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+    func index<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? 
+    {
         range(of: string, options: options)?.lowerBound
     }
-    func endIndex<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+    func endIndex<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? 
+    {
         range(of: string, options: options)?.upperBound
     }
-    func indices<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Index] {
+    func indices<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Index] 
+    {
         ranges(of: string, options: options).map(\.lowerBound)
     }
-    func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>] {
+    func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>] 
+    {
         var result: [Range<Index>] = []
         var startIndex = self.startIndex
         while startIndex < endIndex,
@@ -21,5 +25,11 @@ extension StringProtocol {
                     index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
         }
         return result
+    }
+    func regex(of query:String) -> Bool
+    {
+        let escapedQuery = NSRegularExpression.escapedPattern(for: query)
+        let ranges = self.ranges(of: escapedQuery, options: .regularExpression)
+        return ranges.count > 0
     }
 }
